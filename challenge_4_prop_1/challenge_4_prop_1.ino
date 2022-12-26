c5319a4ad6c124a1e87e079f4ca1cd9bc1f21956
Niels Minne <67301518+NielsMinne@users.noreply.github.com>
2022-12-26 15:21:36 +0100

    New Thermometer code

--- challenge_4_prop_1/challenge_4_prop_1.ino
+++ challenge_4_prop_1/challenge_4_prop_1.ino
@@ -8,7 +8,7 @@
 WifiProp prop(u8"challenge_4_prop_1", // as MQTT client id, should be unique per client for given broker
                   u8"Room/My room/Props/challenge_4_prop_1/inbox",
                   u8"Room/My room/Props/challenge_4_prop_1/outbox",
-                  "192.168.50.101", // your MQTT server IP address1
+                  "192.168.50.101", // your MQTT server IP address
                   1883); // your MQTT server port;
 
 /* ---------------
@@ -26,16 +26,32 @@ PropAction morse = PropAction(0, playMorse);
 bool wifiBegun(false); // this is an internal variable to use
 const char* ssid = "WOT"; // the SSID of the network
 const char *passphrase = "enterthegame"; // the passphrase of the network
-const int propNumber = 161;
+const int propNumber = 141;
 
 // morse variables
+bool lightsOn = false;
 bool videoIsSynced = false;
-const int syncTime = 1000; // time that needs to pass till program is synced with video
+bool rfidSolved = false;
 const int relay = 7;
-char morseCode[] = "--- .-. .-- . .-.. .-.."; // ORWELL in morse code
-unsigned long currentTime = 0;
-bool videoSync = false;
 bool ventilationIsOpen = false;
+bool gameStopped = false;
+
+//Variables (Niels)
+unsigned long FRIDA[][4] = {{500, 500, 2000, 500}, {500, 2000,500,0}, {500,500,0,0},{2000,500,500,0},{500,2000,0,0}}; //The sequence of the word Frida
+unsigned int silence = 500; //time between intervals
+unsigned int nextLetter = 5000; // time between letters
+unsigned int nextWord = 5000; //time between words ( + between letters = 7000)
+int numOfFlashes = 5; //amount of flashes for modulo
+int numOfLetters = 5; //amount of letters for modulo
+unsigned int index = 0; //index for Frida[index][]
+unsigned int index2 = 0; // index for Frida [][index2]
+unsigned long lastMillis;
+unsigned long waitTime;
+unsigned long currentMillis;
+bool flash;
+unsigned long currentMillis2;
+unsigned long lastMillis2;
+unsigned long waitTimeVideo = 52000;
 
 /* ---------------
  * Sketch Helpper Functions
@@ -64,9 +80,6 @@ void setup()
   prop.addData(&propVideoSync);
   prop.addData(&propVentilationIsOpen);
   prop.addData(&rssi);
-  prop.addData(&isReset);
-  isReset.setValue(true);
-  prop.sendAllData();
 
   prop.begin(InboxMessage::run); // this will start a loop to check for MQTT messages
 
@@ -75,7 +88,14 @@ void setup()
   // morse
   pinMode(relay, OUTPUT);
 
-
+  // wait till synced with timing in video
+  // if (videoSync) {
+  //   currentTime = millis();
+  //   do {
+  //     // Waiting for time to pass...
+  //   } while (millis() < currentTime + syncTime);
+  //   videoIsSynced = true;
+  // }
 
 }
 
@@ -126,20 +146,19 @@ void loop()
   prop.loop(); // start looping the prop (superpowers gottin from xcape.io)
   rssi.setValue(WiFi.RSSI() + String(" dBm")); // https://www.metageek.com/training/resources/understanding-rssi.html
 
-  // wait till synced with timing in video
-  if (videoSync) {
-    isReset.setValue(false);
-    prop.sendAllData();
-    currentTime = millis();
-    do {
-      // Waiting for time to pass...
-    } while (millis() < currentTime + syncTime);
-    videoIsSynced = true;
-  }
-  
   // morse
   // loop morse code till the ventilation closes
-  if(videoIsSynced && !ventilationIsOpen) {
+
+// if(rfidSolved){
+//   currentMillis2 = millis();
+
+//   if(currentMillis2 - lastMillis2 >= waitTimeVideo){
+//       videoIsSynced = true;
+//       lastMillis2 = currentMillis2;
+//     }
+// }
+  
+  if(lightsOn && rfidSolved &&!ventilationIsOpen && videoIsSynced) {
     morse.check();
   }
 }
@@ -149,100 +168,51 @@ void loop()
  * --------------- */
 
 void playMorse() {
-  for (int i = 0; i < strlen(morseCode); i++) {
-    currentTime = millis();
-    Serial.println(i);
 
-    switch(morseCode[i]) {    
-      // dot
-      case 46:
-        Serial.print("Dot\n");
-        prop.sendAllData();
+  currentMillis = millis(); //sets time to currentTime
+  
 
-        // get out of do while loop when the vent opens
-        if(ventilationIsOpen) {
-          exit(0);
-        }
+  if (currentMillis - lastMillis >= waitTime) { //checks if time is greater or equal to the waittime of the letter
+    lastMillis = currentMillis; //resets time to current
 
-        digitalWrite(relay, HIGH);
-
-        do {
-          // Waiting for time to pass...
-          // Light on for 1s
-        } while (millis() < currentTime + 1000);
-
-        currentTime = millis();
-
+    if (flash) { // if the light is turned on, turn off again and wait for the duration of the silence variable
+      flash = false;
+      digitalWrite(relay, LOW);
+      waitTime = silence;
+    } else { // if the light is off, check conditions or else turn the led on
+      if((index2 + 1) % numOfFlashes == 0){ // if the index is at it's end -> no light and put a waittime for the next letter
         digitalWrite(relay, LOW);
+        waitTime = nextLetter;
+        index = (index + 1) % numOfLetters; //increase index for the next letter
+      }
 
-        do {
-          // Waiting for time to pass...
-          // Light off for 1s
-        } while (millis() < currentTime + 1000);
-
-        currentTime = millis();
-
-        break;
-
-      // dash
-      case 45:
-        Serial.print("Dash\n");
-        prop.sendAllData();
-
-        // get out of do while loop when the vent opens
-        if(ventilationIsOpen) {
-          exit(0);
-        }
-
-        digitalWrite(relay, HIGH);
-
-        do {
-          // Waiting for time to pass...
-          // Light on for 3s
-        } while (millis() < currentTime + 3000);
-
-        currentTime = millis();
-
+      flash = true; //set flash to true (see above)
+      waitTime = FRIDA[index][index2]; // set the waittime equal to the array value of FRIDA [][]
+      index2 = (index2 + 1) % numOfFlashes; // increase the index to go to the next dot or dash
+      
+      if(index == 4 && index2 == 4){ // if we are at the end of the word and interval -> add waitTime to start again
+          waitTime = nextWord;
+      }
+      else if(index2 == 0){ // if we are at the end of a letter sequence, set light off and waitTime for next letter
         digitalWrite(relay, LOW);
-
-        do {
-          // Waiting for time to pass...
-          // Light off for 1s
-        } while (millis() < currentTime + 1000);
-
-        currentTime = millis();
-
-        break;
-      // space
-      case 32:
-        // space between words is 3s -> 1s is already default after every dot or dash
-        Serial.print("Space\n");
-        prop.sendAllData();
-
-        // get out of do while loop when the vent opens
-        if(ventilationIsOpen) {
-          exit(0);
-        }
-
-        do {
-          // Waiting for time to pass...
-          // Light off for 2s
-        } while (millis() < currentTime + 2000);
-
-        currentTime = millis();
-
-        break;
-      default:     
-        exit(0);
+        waitTime = nextLetter;
+      }
+      else if(waitTime == 0){ // if the waitTime in FRIDA [][] == 0 , No light should be turned on
+        digitalWrite(relay, LOW);
+      }
+      else{
+        digitalWrite(relay, HIGH); //turns on the led if other conditions are not fulfilled -> sequence
+      }
+    
     }
   }
-  // space between words is 7s -> 1s is already default after every dot or dash
-  Serial.print("Between letters\n");
-  prop.sendAllData();
-  do {
-    // Waiting for time to pass...
-    // Light on for 3s
-  } while (millis() < currentTime + 6000);
+
+  // get out of do while loop when the vent opens
+  if (ventilationIsOpen)
+  {
+    exit(0);
+  }
+  
 }
 
 void InboxMessage::run(String a) {
@@ -254,13 +224,17 @@ void InboxMessage::run(String a) {
   }
   else if (a == u8"reset-mcu")
   {
-    prop.resetMcu(); 
+    prop.resetMcu();
   }
-  else if (a == "rfidSolved:true")
+    else if (a == "lightsOn:true")
+  {
+    lightsOn = true;
+  }
+  else if (a == "videoSync:true")
   {
     propVideoSync.setValue(true);
 
-    videoSync = true;
+    videoIsSynced = true;
 
     prop.sendAllData(); // all data change, we don't have to be selective then
     prop.sendDone(a); // acknowledge prop command action
@@ -268,20 +242,42 @@ void InboxMessage::run(String a) {
   else if (a == "ventOpen:true")
   {
     propVentilationIsOpen.setValue(true);
-
+    digitalWrite(relay,LOW);
     ventilationIsOpen = true;
-
+    rfidSolved = false;
+    lightsOn = false;
     prop.sendAllData(); // all data change, we don't have to be selective then
     prop.sendDone(a); // acknowledge prop command action
   }
-  else if (a == "reset")
-  {
-    videoIsSynced = false;
-    videoSync = false;
-    ventilationIsOpen = false;
-    isReset.setValue(true);
+  else if (a == "rfidSolved:true"){
+    
+    propVideoSync.setValue(true);
+    rfidSolved = true;
     prop.sendAllData();
-    digitalWrite(relay, LOW);
+    prop.sendDone(a);
+  }
+    else if (a == "videoEnded"){
+    videoIsSynced = true;
+    prop.sendAllData();
+    prop.sendDone(a);
+  }
+  else if (a == "reset"){
+    isReset.setValue(true);
+    index = 0;
+    index2 = 0;
+    videoIsSynced = false;
+    rfidSolved = false;
+    lightsOn = false;
+    ventilationIsOpen = false;
+    digitalWrite(relay,LOW);
+    prop.sendAllData();
+  }
+  else if (a == u8"game:end" || a == u8"app:exit")
+  {
+    gameStopped = true;
+
+    prop.sendAllData();
+    prop.sendDone(a);
   }
   else
   {
